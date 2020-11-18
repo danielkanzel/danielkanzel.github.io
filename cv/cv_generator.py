98d37486411dd0f3c373f382b896e6f0769c888f
Daniel <danersow@gmail.com>
2020-11-18 17:13:33 +0300

    nope

--- cv/cv_generator.py
+++ cv/cv_generator.py
@@ -8,6 +8,6 @@ me = json.loads(open("cv/cv.json",encoding="utf-8").read())
 template = Template(open('cv/template.html', encoding="utf-8").read())
 
 
-f = open("../index.html", "w", encoding="utf-8")
+f = open("./index.html", "w", encoding="utf-8")
 f.write(bs(template.render(person=me), 'html.parser').prettify())
 f.close()
