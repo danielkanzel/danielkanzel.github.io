89ef9ebeba0915b17d4046f5cadf13e56c1bcca2
Daniel <danersow@gmail.com>
2020-11-21 22:59:38 +0300

    wtf goin on

--- cv_generator.py
+++ cv_generator.py
@@ -3,9 +3,9 @@ import json
 from bs4 import BeautifulSoup as bs
 
 
-me = json.loads(open("cv/cv.json",encoding="utf-8").read())
+me = json.loads(open("cv_src.json",encoding="utf-8").read())
 
-template = Template(open('cv/template.html', encoding="utf-8").read())
+template = Template(open('template.html', encoding="utf-8").read())
 
 
 f = open("index.html", "w", encoding="utf-8")
